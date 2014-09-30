14f32ba6d2cafa398fb8a10824c2967107dcd369
Alexander Allan <45302@student.longroad.ac.uk>
2014-09-30 15:15:55 +0100

    Commited.

---
+++ ACSII Exercise _ Task 1.py
@@ -0,0 +1,7 @@
+#Alexander Allan
+#30/09/2014
+#ACSII theory excersise Task 1
+
+Choice = int(input("Please choose what you would like to do, and enter the number. (1) = Convert ACSII code to text character. (2) Convert a text character to ACSII code.")
+if choice ==1
+            ACSII_Code_1 = int(input("Please enter the ACSII code : "))
